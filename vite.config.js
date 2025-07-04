34a1288d7ab8edc8cfad79c511f0b7d05b7b58a1
nifemi-l <lawalwaryth@gmail.com>
2025-07-04 04:26:57 -0500

    Modift config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 4173,
-    allowedHosts: 'all'
+    strictPort: true,
+    allowedHosts: [
+      'schwinsight.onrender.com',
+      'localhost',
+      '127.0.0.1'
+    ]
   }
 })
